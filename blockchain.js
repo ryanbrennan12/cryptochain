2f25d504738f0c7c10d7ca772670f72592252147
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-21 16:14:17 -0700

    [add] errorMock/ logMock:wq

--- blockchain.js
+++ blockchain.js
@@ -18,11 +18,9 @@ class Blockchain {
     if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) {
       return false;
     }
-
-    for (let i=1; i<chain.length; i++) {
+    for (let i = 1; i < chain.length; i ++) {
       const { timestamp, lastHash, hash, nonce, difficulty, data } = chain[i];
-      const actualLastHash = chain[i-1].hash;
-      const lastDifficulty = chain[i-1].difficulty;
+      const actualLastHash = chain[i - 1].hash;
 
       if (lastHash !== actualLastHash) return false;
 
@@ -31,6 +29,20 @@ class Blockchain {
     }
 
     return true;
+  }
+
+
+  replaceChain(chain) {
+    if (chain.length <= this.chain.length) {
+      console.error('The incoming chain must be longer');
+      return;
+    }
+    if(!Blockchain.isValidChain(chain)) {
+      console.error('The incoming chain must be valid');
+      return;
+    };
+    console.log('replacing CHAIN with ', chain)
+    this.chain = chain;
   }
 
 }
--- blockchain.test.js
+++ blockchain.test.js
@@ -2,27 +2,28 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-  let blockchain;
+  let blockchain, newChain, originalChain;
 
   beforeEach(() => {
     blockchain = new Blockchain();
+    newChain = new Blockchain();
 
+    originalChain = blockchain.chain;
   })
 
   it('contains a `chain` Array instance', () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   });
+
   it('starts with the genesis block', () => {
     expect(blockchain.chain[0]).toEqual(Block.genesis());
   });
-
 
   it('adds a new block to the chain', () => {
     const newData = 'rye bread';
     blockchain.addBlock({ data: newData });
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
   });
-
 
   describe('isValidChain()', () => {
 
@@ -60,6 +61,87 @@ describe('Blockchain', () => {
       });
     });
   });
+
+  describe('replaceChain()', () => {
+    let logMock, errorMock;
+
+    beforeEach(() => {
+      logMock = jest.fn();
+      errorMock = jest.fn();
+
+      global.console.log = logMock;
+      global.console.error = errorMock;
+    });
+
+    describe('when the new chain is not longer', () => {
+      beforeEach(() => {
+        newChain.chain[0] = { new: 'chain' };
+
+        blockchain.replaceChain(newChain.chain);
+      });
+
+      it('does not replace the chain', () => {
+        expect(blockchain.chain).toEqual(originalChain);
+      });
+
+      it('logs an error', () => {
+        expect(errorMock).toHaveBeenCalled();
+      });
+    });
+
+    describe('when the new chain IS LONGER', () => {
+      beforeEach(() => {
+        newChain.addBlock({ data: 'Bears' });
+        newChain.addBlock({ data: 'Beets' });
+        newChain.addBlock({ data: 'Battlestar Galactica' });
+      });
+
+      describe('and the chain is invalid', () => {
+        beforeEach(() => {
+          newChain.chain[2].hash = 'some-fake-hash';
+
+          blockchain.replaceChain(newChain.chain);
+        });
+
+        it('does not replace the chain', () => {
+          expect(blockchain.chain).toEqual(originalChain);
+        });
+
+        it('logs an error', () => {
+          expect(errorMock).toHaveBeenCalled();
+        });
+      });
+
+      describe('and the chain is valid', () => {
+        beforeEach(() => {
+          blockchain.replaceChain(newChain.chain);
+        });
+
+        it('replaces the chain', () => {
+          expect(blockchain.chain).toEqual(newChain.chain);
+        });
+
+        it('logs about the chain replacement', () => {
+          expect(logMock).toHaveBeenCalled();
+        });
+      });
+    });
+
+    // describe('and the `validateTransactions` flag is true', () => {
+    //   it('calls validTransactionData()', () => {
+    //     const validTransactionDataMock = jest.fn();
+
+    //     blockchain.validTransactionData = validTransactionDataMock;
+
+    //     newChain.addBlock({ data: 'foo' });
+    //     blockchain.replaceChain(newChain.chain, true);
+
+    //     expect(validTransactionDataMock).toHaveBeenCalled();
+    //   });
+    // });
+  });
 });
+
+
 
 
