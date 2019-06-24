b5ab783b5585622512f37c8f412655a4579df429
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-24 15:39:05 -0700

    init wallet balance and public key

--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,17 @@
-class Wallet {};
+const { STARTING_BALANCE } = require('../config');
+const { ec } = require('../util');
+
+
+class Wallet {
+  constructor() {
+    this.balance = STARTING_BALANCE
+
+    const keyPair = ec.genKeyPair();
+
+    this.publicKey = keyPair.getPublic().encode('');
+  }
+}
+
+
 
 module.exports = Wallet;
