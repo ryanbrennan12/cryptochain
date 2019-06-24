6783e61f3bc39e9f0f4083d4acc634727565c35f
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-23 19:54:40 -0700

    express server and initial routes

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const app = express();
+const blockchain = new Blockchain();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/api/blocks', (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  console.log(req.body)
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+  res.redirect('/api/blocks');
+});
+
+app.listen(port, () => {
+  console.log(`We listening at localhost:${port}`);
+})
+
