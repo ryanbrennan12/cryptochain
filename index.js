7ca5124724629d53cc53b3972926731766857db3
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-23 21:01:17 -0700

    redis functioning new script

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 
+
 const app = express();
 const blockchain = new Blockchain();
 const port = 3000;
