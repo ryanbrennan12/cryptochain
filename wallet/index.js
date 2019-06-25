627e2bc48945852f14ba78d3633b3840ed75fff4
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-24 18:22:34 -0700

    marking data w/signatures and validating signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,17 +1,23 @@
 const { STARTING_BALANCE } = require('../config');
-const { ec } = require('../util');
+const { ec, verifySignature } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
 
 
 class Wallet {
   constructor() {
     this.balance = STARTING_BALANCE
+    this.keyPair = ec.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('');
+  }
 
-    const keyPair = ec.genKeyPair();
-
-    this.publicKey = keyPair.getPublic().encode('');
+  sign(data) {
+    return this.keyPair.sign(cryptoHash(data));
   }
 }
 
-
-
 module.exports = Wallet;
+
+
+
+
+
