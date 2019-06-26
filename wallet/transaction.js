5cd6b66a5d3d95c772d08456aafb02dbf988f44e
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-06-26 15:40:03 -0700

    update function adge cases

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -60,12 +60,21 @@ class Transaction {
   }
 
   update({ senderWallet, recipient, amount }) {
-    this.outputMap[recipient] = amount
+    if(amount > this.outputMap[senderWallet.publicKey]) {
+      throw new Error('AMOUNT EXCEEDS BALANCE!!')
+    }
+
+    if(!this.outputMap[recipient]) {
+      this.outputMap[recipient] = amount;
+    } else {
+      this.outputMap[recipient] = this.outputMap[recipient] + amount;
+    }
 
     this.outputMap[senderWallet.publicKey] =
     this.outputMap[senderWallet.publicKey] - amount;
 
-    this.input = this.createInput({ senderWallet, outputMap: this.outputMap })
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
+
   }
 }
 
