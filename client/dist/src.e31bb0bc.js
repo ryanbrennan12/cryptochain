6f7f508ca85cebeeaf39f9be638795a9056888f3
Ryan Brennan <ryanbrennanbc1@gmail.com>
2019-07-11 17:41:32 -0700

    change UI

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -47635,7 +47635,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58787" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59674" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
